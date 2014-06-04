5e6808daac4c2e00bf8c00c88784253acd245431
whyisandroid <583417398@qq.com>
2014-06-04 17:40:12 +0800

    修改数字改变算法

--- src/com/why/animation/MainActivity.java
+++ src/com/why/animation/MainActivity.java
@@ -61,15 +61,15 @@ public class MainActivity extends Activity implements OnClickListener {
 		@Override
 		public void run() {
     		int numStr = Integer.valueOf(tv_num.getText().toString());
-    		
+     		
     		// 求余 处理 防止出现 根本停不下来情况
-			if ((numStr - value) % ADD != 0) {
-				int remainder = numStr > value ? numStr + value % ADD : value + numStr % ADD;
+    		if ((numStr - value) % ADD != 0) {
+				int remainder = numStr < value ? numStr + (value - numStr)% ADD : numStr + (numStr-value) % ADD;
 				tv_num.setText(remainder + "");
 				handler.postDelayed(run, delayMillis);
 				return;
 			}
-    		
+    		 
     		if(numStr > value){
     			tv_num.setText(numStr-ADD+"");
 	    		handler.postDelayed(run, delayMillis);
